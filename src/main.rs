78c2483b505893157857a279af767d6440cbf769
Aaron Mineen <amineen64@hotmail.com>
2024-03-29 11:13:29 +0200

    Refactor code to use array instead of vector and measure execution time

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![allow(unused)]
 
-use core::num;
+use std::time::Instant;
 fn main() {
-    let mut numbers = vec![1, 2, 3, 4, 5];
+    let start = Instant::now();
+    // let mut numbers = vec![1, 2, 3, 4, 5];
+    let numbers = [1, 2, 3, 4, 5];
     let numbers_squared: Vec<i32> = numbers.iter().map(|x| x * x).collect();
-    numbers.push(6);
+    // numbers.push(6);
     println!("{:?}", numbers_squared);
+
+    let duration = start.elapsed();
+
+    println!("Time: {:?}", duration);
 }
